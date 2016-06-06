0c2fc76a0732d11097cc5ee6f272c176ac931730
Justin Fitzsimmons <justinf@nulogy.com>
2016-06-06 00:30:25 -0400

    Removes test code so that page loads in app are not confusing

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,11 +1,3 @@
 if (typeof browser === 'undefined' && chrome) {
     browser = chrome;
 }
-
-browser.runtime.sendMessage({
-        url: "https://www.bungie.net/Platform/Destiny/2/Account/4611686018452889968/Character/2305843009333785046/Advisors/"
-    },
-    function(data) {
-        console.log(data);
-    }
-);
