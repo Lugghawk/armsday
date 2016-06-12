b1e1d6fcdd6adcb6400a02882de36cff5a00173a
Justin Fitzsimmons <justinf@nulogy.com>
2016-06-12 12:04:29 -0400

    Proof of concept for handling redemptions in subprocess

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -6,41 +6,41 @@ defmodule Armsday.RoomChannel do
   end
 
   def handle_in("redemptions_start", _, socket) do
-    socket = psn_id(socket, fn psn_id ->
-      IO.puts "psn_id: #{psn_id}"
-      membership_id = Armsday.Destiny.membership_id(psn_id)
-      IO.puts "membership_id: #{membership_id}"
-      characters = Armsday.Destiny.character_ids(membership_id)
-      IO.puts "characters"
-      IO.inspect(characters)
-    end)
+    socket = handle_redemptions(socket)
     {:noreply, socket}
   end
 
   def handle_in("privileged_bungie_response", %{"url" => url, "response" => response}, socket) do
-    IO.puts "Socket assigns:"
-    IO.inspect socket.assigns
-
-    send socket.assigns[url], {url, response}
+    send socket.assigns[:replyto], {url, response}
     {:noreply, socket}
   end
 
-  defp psn_id(socket, callback) do
-    socket = privileged_bungie("https://www.bungie.net/Platform/User/GetBungieNetUser/", socket, fn response, socket ->
-        callback.(response["Response"]["psnId"])
+  defp handle_redemptions(socket) do
+    child_pid = spawn_link(fn ->
+      username = psn_id(socket)
+      membership_id = Armsday.Destiny.membership_id(username)
+      characters = Armsday.Destiny.character_ids(membership_id)
+      IO.puts("#{username} is membership_id #{membership_id} and their characters are")
+      IO.inspect(characters)
+      redemption = redemptions(membership_id, hd(characters), socket)
+      IO.inspect redemption
     end)
+    socket = assign(socket, :replyto, child_pid)
     socket
   end
 
-  defp privileged_bungie(url, socket, callback) do
-    pid = spawn_link fn ->
-      push socket, "privileged_bungie", %{url: url}
-      response = receive do
-        {^url, response} -> response
-      end
-      callback.(response, socket)
+  defp psn_id(socket) do
+    privileged_bungie("https://www.bungie.net/Platform/User/GetBungieNetUser/", socket)["Response"]["psnId"]
+  end
+
+  defp redemptions(membership_id, character_id, socket) do
+    redemptions = privileged_bungie("https://www.bungie.net/Platform/Destiny/2/Account/#{URI.encode(membership_id)}/Character/#{URI.encode(character_id)}/Advisors/", socket)["Response"]["data"]["armsDay"]["redemptions"] |> Map.values
+  end
+
+  defp privileged_bungie(url, socket) do
+    push socket, "privileged_bungie", %{url: url}
+    receive do
+      {^url, response} -> response
     end
-    socket = assign(socket, url, pid)
-    socket
   end
 end
